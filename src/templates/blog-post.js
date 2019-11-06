e935fc1b8a8f52f73223622b46fc6ec7d7f968ff
Coner Murphy <conermurphy@gmail.com>
2019-11-06 20:49:45 +0000

    Reset blog post styling back to original

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from '../components/layout'
 import blogPostStyles from "../styles/blogPost.module.css"
 import {FaArrowLeft} from 'react-icons/fa';
 
@@ -7,16 +8,14 @@ import {FaArrowLeft} from 'react-icons/fa';
 export default ({ data }) => {
     const post = data.markdownRemark;
     return (
-         <div className={blogPostStyles.all}>
 
-            
+        <Layout>
             <div className={blogPostStyles.container}> 
-              <div className={blogPostStyles.backarrow}><button onClick={() => window.history.back()}><FaArrowLeft/><h5> Back to Main Page.</h5></button></div>
+              <div className={blogPostStyles.backarrow}><button onClick={() => window.history.back()}><FaArrowLeft/><p> Back to the blog.</p></button></div>
               <div className={blogPostStyles.content} dangerouslySetInnerHTML={{ __html: post.html }}/>
-              <div className={blogPostStyles.backarrow}><button onClick={() => window.history.back()}><FaArrowLeft/><h5> Back to Main Page.</h5></button></div>
+              <div className={blogPostStyles.backarrow}><button onClick={() => window.history.back()}><FaArrowLeft/><p> Back to the blog.</p></button></div>
             </div>
-
-        </div>
+        </Layout>
   )
 }
 
