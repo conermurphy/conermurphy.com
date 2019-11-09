87b7782160a20815abf722dd8ae2f5721997a87d
Coner Murphy <conermurphy@gmail.com>
2019-11-09 22:10:20 +0000

    Added share button on blog posts

---
+++ src/components/shareMenu.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import {FaShareAlt, FaTwitter, FaInstagram} from 'react-icons/fa'
+import styled from 'styled-components'
+
+const ShareContainer = styled.section`
+
+`
+
+const ShareButton = styled.a`
+    display: flex;
+    position: fixed;
+    align-items: center;
+    justify-content: center;
+    width: 3.5rem;
+    height: 3.5rem;
+    left: 20%;
+    top: 20%;
+    z-index: 3;
+    color: #1f2a51;
+    background-color: white;
+    border: 2px solid #1f2a51;
+    transition: 0.2s;
+
+    :hover  {
+        background-color: #1f2a51;
+        color: white;
+    }
+`
+
+const ShareIcon = styled.a`
+    z-index: 1;
+    width: 3.5rem;
+    height: 3.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid #1f2a51;
+    color: #1f2a51;
+    background-color: white;
+    text-align: center;
+    position: fixed;
+    left: 20%;
+    top: 20%;
+
+    :hover  {
+        background-color: #1f2a51;
+        color: white;
+    }
+
+    :nth-child(2)   {
+        top: ${props => props.shareOpen ? '26%' : '20%'};
+        z-index: 2;
+        transition: 0.25s
+    }
+
+    :nth-child(3)  {
+        top: ${props => props.shareOpen ? '32%' : '20%'};
+        z-index: 1;
+        transition: 0.5s
+    }
+`
+
+class ShareMenu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            shareOpen: false
+        }
+    }
+
+    openShare(e) {
+        this.setState({
+            shareOpen: !this.state.shareOpen
+        })
+    }
+
+    render() {
+    return (
+        <ShareContainer>
+              <ShareButton onClick={this.openShare.bind(this)}><FaShareAlt/></ShareButton>
+              <ShareIcon shareOpen={this.state.shareOpen}><FaTwitter/></ShareIcon>
+              <ShareIcon shareOpen={this.state.shareOpen}><FaInstagram/></ShareIcon>
+        </ShareContainer>
+    )
+    }
+}
+
+export default ShareMenu
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,6 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Img from 'gatsby-image'
 import Layout from '../components/layout'
+import ShareMenu from '../components/shareMenu'
 import blogPostStyles from "../styles/blogPostStyles.module.css"
 import {FaArrowLeft} from 'react-icons/fa';
 
@@ -32,10 +33,11 @@ export default ({ data }) => {
     return (
 
         <Layout>
-            <div className={blogPostStyles.backarrow}><button onClick={() => window.history.back()}><FaArrowLeft/></button></div>
             <div className={blogPostStyles.blogPostDateContainer}>  
               <h4 className={blogPostStyles.blogPostDate}>{data.markdownRemark.frontmatter.date}</h4>
             </div>
+            <div className={blogPostStyles.backarrow}><button onClick={() => window.history.back()}><FaArrowLeft/></button></div>
+            <ShareMenu/>
             <div className={blogPostStyles.container}>
               <Img style={{width:`100%`, borderRadius:`2rem`, marginBottom:`2rem`}} fluid={data.markdownRemark.frontmatter.featuredImage.childImageSharp.fluid}/> 
               <div className={blogPostStyles.blogPostIDCategoryContainer}>
