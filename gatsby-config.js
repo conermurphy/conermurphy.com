b917da21220ce2d37362c9254b56ecad84c6ca8e
Coner Murphy <coner@conermurphy.com>
2020-01-03 18:32:21 +0000

    Added RSS XML feed for cross posting

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `Coner Murphy`,
     description: "The Web Made Simple.",
     author: `Coner Murphy`,
+    siteUrl: `https://conermurphy.com`
   },
   plugins: [
     {
@@ -78,6 +79,66 @@ module.exports = {
   `gatsby-transformer-sharp`,
   `gatsby-plugin-styled-components`,
   `gatsby-plugin-react-helmet`,
-  `gatsby-plugin-offline`
+  `gatsby-plugin-offline`,
+  {
+    resolve: `gatsby-plugin-feed`,
+    options: {
+      query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+      // feeds: [
+      //   {
+      //     serialize: ({ query: { site, allMarkdownRemark } }) => {
+      //       return allMarkdownRemark.edges.map(edge => {
+      //         return Object.assign({}, edge.node.frontmatter, {
+      //           description: edge.node.excerpt,
+      //           date: edge.node.frontmatter.date,
+      //           url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+      //           guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+      //           custom_elements: [{ "content:encoded": edge.node.html }],
+      //         })
+      //       })
+      //     },
+      //     query: `
+      //       {
+      //         allMarkdownRemark(
+      //           sort: { order: DESC, fields: [frontmatter___date] },
+      //         ) {
+      //           edges {
+      //             node {
+      //               excerpt
+      //               html
+      //               fields { slug }
+      //               frontmatter {
+      //                 title
+      //                 date
+      //               }
+      //             }
+      //           }
+      //         }
+      //       }
+      //     `,
+      //     output: "/rss.xml",
+      //     title: "Your Site's RSS Feed",
+      //     // optional configuration to insert feed reference in pages:
+      //     // if `string` is used, it will be used to create RegExp and then test if pathname of
+      //     // current page satisfied this regular expression;
+      //     // if not provided or `undefined`, all pages will have feed reference inserted
+      //     match: "^/blog/",
+      //     // optional configuration to specify external rss feed, such as feedburner
+      //     link: "https://feeds.feedburner.com/gatsby/blog",
+      //   },
+      // ],
+    },
+  },
   ],
 }
