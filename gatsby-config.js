ea3ee83c4299f74263a8036d44142949dc4a0d06
Coner Murphy <coner@conermurphy.com>
2020-01-22 16:06:07 +0000

    Redirect changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,89 +1,89 @@
 module.exports = {
   siteMetadata: {
-    title: `Coner Murphy`,
-    description: "The Web Made Simple.",
-    author: `Coner Murphy`,
-    siteUrl: `https://conermurphy.com`
+    title: 'Coner Murphy',
+    description: 'The Web Made Simple.',
+    author: 'Coner Murphy',
+    siteUrl: 'https://conermurphy.com'
   },
   plugins: [
     {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      path: `${__dirname}/content/posts`,
-      name: 'posts'
-    }
-  },
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      path: `${__dirname}/content/media/images`,
-      name: 'images'
-    }
-  },
-  {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      path: `${__dirname}/content/media/assets`,
-      name: 'assets'
-    }
-  },
-  {
-    resolve: `gatsby-transformer-remark`,
-    options: {
-      plugins: [
-        {
-          resolve: `gatsby-remark-images`,
-          options: {
-            maxWidth: 800,
-          },
-        },
-      ],
-    },
-  },
-  `gatsby-transformer-json`,
-    {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        path: `./content/data/`,
-      },
+        path: `${__dirname}/content/posts`,
+        name: 'posts'
+      }
     },
-  {
-    resolve: `gatsby-source-instagram`,
-    options: {
-      username: `conermurphy`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/media/images`,
+        name: 'images'
+      }
     },
-  },
-  {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      name: `Coner Murphy`,
-      short_name: `Coner Murphy`,
-      start_url: `/`,
-      background_color: `#1f2a51`,
-      theme_color: `#1f2a51`,
-      // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-      // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-      lang: `en`,
-      display: `standalone`,
-      icon: `content/media/assets/CM-Logo-2019.svg`, // This path is relative to the root of the site.
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/media/assets`,
+        name: 'assets'
+      }
     },
-  }, 
-  {
-    resolve: `gatsby-plugin-google-analytics`,
-    options: {
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 800
+            }
+          }
+        ]
+      }
+    },
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './content/data/'
+      }
+    },
+    {
+      resolve: 'gatsby-source-instagram',
+      options: {
+        username: 'conermurphy'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Coner Murphy',
+        short_name: 'Coner Murphy',
+        start_url: '/',
+        background_color: '#1f2a51',
+        theme_color: '#1f2a51',
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        lang: 'en',
+        display: 'standalone',
+        icon: 'content/media/assets/CM-Logo-2019.svg' // This path is relative to the root of the site.
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
       // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-      trackingId: "UA-107696124-1",
+        trackingId: 'UA-107696124-1'
+      }
     },
-  },
-  `gatsby-plugin-sharp`,
-  `gatsby-transformer-sharp`,
-  `gatsby-plugin-styled-components`,
-  `gatsby-plugin-react-helmet`,
-  `gatsby-plugin-offline`,
-  {
-    resolve: `gatsby-plugin-feed`,
-    options: {
-      query: `
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
         {
           site {
             siteMetadata {
@@ -94,7 +94,7 @@ module.exports = {
             }
           }
         }
-      `,
+      `
       // feeds: [
       //   {
       //     serialize: ({ query: { site, allMarkdownRemark } }) => {
@@ -138,7 +138,8 @@ module.exports = {
       //     link: "https://feeds.feedburner.com/gatsby/blog",
       //   },
       // ],
+      }
     },
-  },
-  ],
-}
+    'gatsby-plugin-netlify'
+  ]
+};
