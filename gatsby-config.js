154fb9818837e5cc83d3ba6b19a2546fdc3e5d0f
Coner Murphy <coner@conermurphy.com>
2020-04-30 21:51:32 +0100

    Continued basic layout of files

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,12 +48,6 @@ module.exports = {
       }
     },
     {
-      resolve: 'gatsby-source-instagram',
-      options: {
-        username: 'conermurphy'
-      }
-    },
-    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'Coner Murphy',
@@ -61,18 +55,9 @@ module.exports = {
         start_url: '/',
         background_color: '#1f2a51',
         theme_color: '#1f2a51',
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         lang: 'en',
         display: 'standalone',
         icon: 'content/media/assets/CM-Logo-2019.svg' // This path is relative to the root of the site.
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-      // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: 'UA-107696124-1'
       }
     },
     'gatsby-plugin-sharp',
@@ -80,66 +65,5 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-offline',
-    {
-      resolve: 'gatsby-plugin-feed',
-      options: {
-        query: `
-        {
-          site {
-            siteMetadata {
-              title
-              description
-              siteUrl
-              site_url: siteUrl
-            }
-          }
-        }
-      `
-      // feeds: [
-      //   {
-      //     serialize: ({ query: { site, allMarkdownRemark } }) => {
-      //       return allMarkdownRemark.edges.map(edge => {
-      //         return Object.assign({}, edge.node.frontmatter, {
-      //           description: edge.node.excerpt,
-      //           date: edge.node.frontmatter.date,
-      //           url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-      //           guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-      //           custom_elements: [{ "content:encoded": edge.node.html }],
-      //         })
-      //       })
-      //     },
-      //     query: `
-      //       {
-      //         allMarkdownRemark(
-      //           sort: { order: DESC, fields: [frontmatter___date] },
-      //         ) {
-      //           edges {
-      //             node {
-      //               excerpt
-      //               html
-      //               fields { slug }
-      //               frontmatter {
-      //                 title
-      //                 date
-      //               }
-      //             }
-      //           }
-      //         }
-      //       }
-      //     `,
-      //     output: "/rss.xml",
-      //     title: "Your Site's RSS Feed",
-      //     // optional configuration to insert feed reference in pages:
-      //     // if `string` is used, it will be used to create RegExp and then test if pathname of
-      //     // current page satisfied this regular expression;
-      //     // if not provided or `undefined`, all pages will have feed reference inserted
-      //     match: "^/blog/",
-      //     // optional configuration to specify external rss feed, such as feedburner
-      //     link: "https://feeds.feedburner.com/gatsby/blog",
-      //   },
-      // ],
-      }
-    },
-    'gatsby-plugin-netlify'
   ]
 };
