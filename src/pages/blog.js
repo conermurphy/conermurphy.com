95881172c5122c254821dfd5fa1bcd61f51bd478
Coner Murphy <coner@conermurphy.com>
2020-05-01 11:12:19 +0100

    changed files to resolve graphql errors

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -18,56 +18,6 @@ const BlogContainerOuter = styled.div`
   }
 `;
 
-const Blog = () => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        writing: allMarkdownRemark(
-          sort: { order: DESC, fields: frontmatter___date }
-          filter: { fileAbsolutePath: { regex: "/(posts)/.*\\\\.md$/" } }
-        ) {
-          edges {
-            node {
-              id
-              frontmatter {
-                date(formatString: "DDMMYYYY")
-                title
-                description
-                id
-                authorid
-                languages
-                category
-                tags
-              }
-              fields {
-                slug
-              }
-              excerpt
-              timeToRead
-            }
-          }
-        }
-      }
-    `
-  );
-
-  return (
-    <Layout>
-      <SEO title="Coner Murphy | Blog" />
-      <main id="blog">
-        <h2>BLOG</h2>
-        <p>
-          Everything Front-End related. HTML, CSS, JS and more. If you see something interesting or want to request a topic, please{' '}
-          <Link to="/#contact">contact me.</Link>
-        </p>
-        <BlogContainerOuter>
-          {data.writing.edges.map(({ node }) => (
-            <Link to={node.fields.slug} style={{ textDecoration: 'none' }} key={node.id}></Link>
-          ))}
-        </BlogContainerOuter>
-      </main>
-    </Layout>
-  );
-};
+const Blog = () => <h1>Hello World Blog!</h1>;
 
 export default Blog;
