b84627205a74372a30203fc2e5435742f9f9a9b6
Coner Murphy <coner@conermurphy.com>
2020-05-01 10:52:32 +0100

    Setup MDX Files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,31 @@
 module.exports = {
   siteMetadata: {
     title: 'Coner Murphy',
-    description: 'The Web Made Simple.',
+    description: 'Web Developer | Blogger | Content Creator.',
     author: 'Coner Murphy',
-    siteUrl: 'https://conermurphy.com'
+    siteUrl: 'https://conermurphy.com',
   },
   plugins: [
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/posts`,
-        name: 'posts'
-      }
+        name: 'posts',
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/media/images`,
-        name: 'images'
-      }
+        name: 'images',
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/media/assets`,
-        name: 'assets'
-      }
+        name: 'assets',
+      },
     },
     {
       resolve: 'gatsby-transformer-remark',
@@ -34,18 +34,26 @@ module.exports = {
           {
             resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 800
-            }
-          }
-        ]
-      }
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
     },
     'gatsby-transformer-json',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: './content/data/'
-      }
+        path: './content/data/portfolio',
+        name: 'portfolio',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './content/data/testimonials',
+        name: 'testimonials',
+      },
     },
     {
       resolve: 'gatsby-plugin-manifest',
@@ -57,13 +65,13 @@ module.exports = {
         theme_color: '#1f2a51',
         lang: 'en',
         display: 'standalone',
-        icon: 'content/media/assets/CM-Logo-2019.svg' // This path is relative to the root of the site.
-      }
+        icon: 'content/media/assets/CM-Logo-2019.svg', // Change this at a later point
+      },
     },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-offline',
-  ]
+  ],
 };
