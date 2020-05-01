75ad2f74bd8dfcb052368320a9042b4f59fdc164
Coner Murphy <coner@conermurphy.com>
2020-05-01 22:11:19 +0100

    added props control of the svgs

--- src/components/cornerArt.js
+++ src/components/cornerArt.js
@@ -8,23 +8,24 @@ const SVGContainer = styled.svg`
   transform: rotate(${props => props.rotation}deg);
 `;
 
-const CornerArt = ([multiplier = 1, rotation = 0, bottom = 0, right = 0]) => (
-  // <SVGContainer>
-  <SVGContainer height={100 * multiplier} width={100 * multiplier} rotation={rotation} bottom={bottom} right={right}>
-    <polygon
-      points={`${100 * multiplier},${20 * multiplier},${100 * multiplier},${100 * multiplier},${80 * multiplier},${100 * multiplier},`}
-    />
-    <polygon
-      points={`${100 * multiplier},${40 * multiplier},${100 * multiplier},${100 * multiplier},${60 * multiplier},${100 * multiplier},`}
-    />
-    <polygon
-      points={`${100 * multiplier},${60 * multiplier},${100 * multiplier},${100 * multiplier},${40 * multiplier},${100 * multiplier},`}
-    />
-    <polygon
-      points={`${100 * multiplier},${80 * multiplier},${100 * multiplier},${100 * multiplier},${20 * multiplier},${100 * multiplier},`}
-    />
-  </SVGContainer>
-  // </SVGContainer>
-);
+const CornerArt = ({ adjustments }) => {
+  const [multiplier, rotation, bottom, right] = adjustments;
+  return (
+    <SVGContainer height={100 * multiplier} width={100 * multiplier} rotation={rotation} bottom={bottom} right={right}>
+      <polygon
+        points={`${100 * multiplier},${20 * multiplier},${100 * multiplier},${100 * multiplier},${80 * multiplier},${100 * multiplier},`}
+      />
+      <polygon
+        points={`${100 * multiplier},${40 * multiplier},${100 * multiplier},${100 * multiplier},${60 * multiplier},${100 * multiplier},`}
+      />
+      <polygon
+        points={`${100 * multiplier},${60 * multiplier},${100 * multiplier},${100 * multiplier},${40 * multiplier},${100 * multiplier},`}
+      />
+      <polygon
+        points={`${100 * multiplier},${80 * multiplier},${100 * multiplier},${100 * multiplier},${20 * multiplier},${100 * multiplier},`}
+      />
+    </SVGContainer>
+  );
+};
 
 export default CornerArt;
