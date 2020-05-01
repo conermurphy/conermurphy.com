594ef13f7ef28ce69cdb3af73114bec44ea4f374
Coner Murphy <coner@conermurphy.com>
2020-05-01 11:09:32 +0100

    configured gatsby-node for mdx querying

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,45 +1,49 @@
 const path = require('path');
-const { createFilePath } = require('gatsby-source-filesystem');
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
+exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions;
-  if (node.internal.type === 'MarkdownRemark') {
-    const slug = createFilePath({ node, getNode, basePath: 'pages' });
+  if (node.internal.type === 'Mdx') {
+    const { id } = node.frontmatter;
     createNodeField({
       node,
       name: 'slug',
-      value: slug,
+      value: `/blog/${id}`,
     });
   }
 };
 
-exports.createPages = ({ graphql, actions }) => {
-  // **Note:** The graphql function call returns a Promise
-  // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
-  return graphql(`
-    {
-      allMarkdownRemark {
+  const result = await graphql(`
+    query {
+      allMdx(sort: { order: ASC, fields: frontmatter___date }) {
         edges {
           node {
             fields {
               slug
             }
+            frontmatter {
+              title
+              date(formatString: "DD/MM/YYYY")
+              category
+            }
           }
         }
       }
     }
-  `).then(result => {
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.fields.slug,
-        component: path.resolve('./src/templates/blog-post-template.js'),
-        context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
-          slug: node.fields.slug,
-        },
-      });
+  `);
+
+  const posts = result.data.allMdx.edges;
+
+  posts.forEach(({ node }, index) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve('./src/templates/blogPost.js'),
+      context: {
+        slug: node.fields.slug,
+        prev: index === 0 ? null : posts[index - 1].node,
+        next: index === posts.length - 1 ? null : posts[index + 1].node,
+      },
     });
   });
 };
