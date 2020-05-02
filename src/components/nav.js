25aa99ef2bbf6ad4c1b613728db9dd987128c30d
Coner Murphy <coner@conermurphy.com>
2020-05-02 19:35:50 +0100

    added test file and add animation to open and close nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import { Link } from 'gatsby';
 import { FaPenSquare, FaBriefcase } from 'react-icons/fa';
 import styled from 'styled-components';
 import Logo from './logo';
 
-const NavContainer = styled.nav`
-  display: flex;
+const NavContainer = styled(motion.nav)`
   position: sticky;
   flex: 1;
   justify-content: space-evenly;
@@ -19,6 +19,7 @@ const NavContainer = styled.nav`
   filter: drop-shadow(0 -2px 2px rgba(200, 200, 200, 20));
 
   bottom: 0;
+  display: flex;
 `;
 
 const NavItem = styled(Link)`
@@ -31,18 +32,43 @@ const NavItem = styled(Link)`
   }
 `;
 
-const Nav = () => (
-  <NavContainer>
-    <NavItem to="/">
-      <FaBriefcase />
-    </NavItem>
-    <NavItem to="/">
-      <Logo height="3.75vh" />
-    </NavItem>
-    <NavItem to="/blog">
-      <FaPenSquare />
-    </NavItem>
-  </NavContainer>
-);
+const variants = {
+  open: { opacity: 1, zIndex: 1 },
+  closed: { opacity: 0, zIndex: '-1' },
+};
+
+const Nav = () => {
+  const [active, setActive] = useState(true);
+  const [lastScroll, setLastScroll] = useState(0);
+
+  const handleScroll = () => {
+    const currentScroll = window.scrollY;
+
+    if (currentScroll < lastScroll || window.pageYOffset === 0) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+    setLastScroll(currentScroll);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  });
+
+  return (
+    <NavContainer animate={active ? 'open' : 'closed'} variants={variants}>
+      <NavItem to="/">
+        <FaBriefcase />
+      </NavItem>
+      <NavItem to="/">
+        <Logo height="3.75vh" />
+      </NavItem>
+      <NavItem to="/blog">
+        <FaPenSquare />
+      </NavItem>
+    </NavContainer>
+  );
+};
 
 export default Nav;
