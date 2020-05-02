7d4eb9e8961dd1adb1d131851015acc2318228f2
Coner Murphy <coner@conermurphy.com>
2020-05-02 17:46:17 +0100

    resolved codeBlock propType error

--- src/components/mdx/codeBlock.js
+++ src/components/mdx/codeBlock.js
@@ -2,24 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 
-const CodeBlock = ({ children }) => (
-  <Highlight {...defaultProps} code={children} language="jsx" theme={undefined}>
-    {({ className, style, tokens, getLineProps, getTokenProps }) => (
-      <pre className={className} style={{ ...style, padding: '20px' }}>
-        {tokens.map((line, i) => (
-          <div key={i} {...getLineProps({ line, key: i })}>
-            {line.map((token, key) => (
-              <span key={key} {...getTokenProps({ token, key })} />
-            ))}
-          </div>
-        ))}
-      </pre>
-    )}
-  </Highlight>
-);
+const CodeBlock = ({ children }) => {
+  console.log(typeof children);
+  return (
+    <Highlight {...defaultProps} code={children} language="jsx" theme={undefined}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: '20px' }}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+};
 
 CodeBlock.propTypes = {
-  children: PropTypes.shape({}).isRequired,
+  children: PropTypes.string.isRequired,
 };
 
 export default CodeBlock;
