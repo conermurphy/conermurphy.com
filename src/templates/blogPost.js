020c9753605bad245b5875fd3f03636498ea930e
Coner Murphy <coner@conermurphy.com>
2020-05-02 17:13:56 +0100

    adding query and prop checking

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -6,13 +6,64 @@ import { MDXProvider } from '@mdx-js/react';
 import PropTypes from 'prop-types';
 import Layout from '../components/layout';
 
-const BlogPost = ({ data }) => {
-  console.log(data);
+const BlogPost = ({ data, pageContext }) => {
+  const post = data.mdx;
+  console.log(post);
   return (
     <div>
       <h1>Hello World</h1>
     </div>
   );
+};
+
+export const query = graphql`
+  query($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      body
+      frontmatter {
+        title
+        description
+        date(formatString: "DDMMYYYY")
+        category
+        languages
+      }
+      timeToRead
+      fields {
+        postId
+      }
+    }
+  }
+`;
+
+BlogPost.propTypes = {
+  data: PropTypes.shape({
+    mdx: PropTypes.shape({
+      body: PropTypes.string.isRequired,
+      frontmatter: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        languages: PropTypes.array.isRequired,
+      }),
+      timeToRead: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
+  pageContext: PropTypes.shape({
+    slug: PropTypes.string.isRequired,
+    prev: PropTypes.shape({
+      fields: PropTypes.shape({
+        slug: PropTypes.string,
+        postId: PropTypes.number.isRequired,
+      }),
+    }),
+    next: PropTypes.shape({
+      fields: PropTypes.shape({
+        slug: PropTypes.string.isRequired,
+        postId: PropTypes.number.isRequired,
+      }),
+    }),
+  }).isRequired,
 };
 
 export default BlogPost;
