3290c4b7c726a731cc798c1e4e965977e57466e1
Coner Murphy <coner@conermurphy.com>
2020-05-03 16:14:05 +0100

    removed console log

--- src/components/mdx/codeBlock.js
+++ src/components/mdx/codeBlock.js
@@ -2,24 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 
-const CodeBlock = ({ children }) => {
-  console.log(typeof children);
-  return (
-    <Highlight {...defaultProps} code={children} language="jsx" theme={undefined}>
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px' }}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </pre>
-      )}
-    </Highlight>
-  );
-};
+const CodeBlock = ({ children }) => (
+  <Highlight {...defaultProps} code={children} language="jsx" theme={undefined}>
+    {({ className, style, tokens, getLineProps, getTokenProps }) => (
+      <pre className={className} style={{ ...style, padding: '20px' }}>
+        {tokens.map((line, i) => (
+          <div key={i} {...getLineProps({ line, key: i })}>
+            {line.map((token, key) => (
+              <span key={key} {...getTokenProps({ token, key })} />
+            ))}
+          </div>
+        ))}
+      </pre>
+    )}
+  </Highlight>
+);
 
 CodeBlock.propTypes = {
   children: PropTypes.string.isRequired,
