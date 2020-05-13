c283531b2081519d9de642803842cd7d6eef0257
Coner Murphy <coner@conermurphy.com>
2020-05-13 17:02:36 +0100

    moved fonts to woff2

--- src/components/fonts.js
+++ src/components/fonts.js
@@ -1,18 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
-const MontserratSemiBold = 'src/assets/fonts/Montserrat-SemiBold.ttf';
-const MontserratRegular = 'src/assets/fonts/Montserrat-Regular.ttf';
+const MontserratSemiBold = 'src/assets/fonts/Montserrat-SemiBold.woff2';
+const MontserratRegular = 'src/assets/fonts/Montserrat-Regular.woff2';
 
 const Fonts = createGlobalStyle`
   @font-face {
     font-family: 'Montserrat';
-    src: url(${MontserratRegular}) format('tff');
+    src: url(${MontserratRegular}) format('woff2');
     font-weight: 400;
     font-style: normal;
   }
   @font-face {
     font-family: 'Montserrat';
-    src: url(${MontserratSemiBold}) format('tff');
+    src: url(${MontserratSemiBold}) format('woff2');
     font-weight: 600;
     font-style: normal;
   }
