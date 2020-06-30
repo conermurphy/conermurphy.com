774b713621c9731ca67455387d1f020fead3dd75
Coner Murphy <coner@conermurphy.com>
2020-06-30 21:19:21 +0100

    laid out base file for new tag generator

---
+++ src/components/templates/tagGenerator.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import tagJSON from '../../data/tags.json';
+
+const StyledTag = styled.p`
+  padding: 0.25rem 0.5rem;
+  margin: 0.25rem 0.5rem;
+  border-radius: 10px;
+  font-weight: 600;
+  background-color: ${props => props.backgroundColor};
+  box-shadow: 0px 2px 2px var(--drop-shadows);
+`;
+
+const TagGenerator = ({ language }) => {
+  const languageTrim = language.trim();
+  const languageToDisplay = tagJSON[languageTrim];
+
+  return <StyledTag backgroundColor={languageToDisplay.backgroundColor}>#{languageTrim}</StyledTag>;
+};
+
+export default TagGenerator;
+
+TagGenerator.propTypes = {
+  language: PropTypes.string.isRequired,
+};
