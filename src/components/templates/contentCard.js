49ea327dc3e9b9e32c4d8b7a7cc49b054b14428c
Coner Murphy <coner@conermurphy.com>
2020-07-10 11:14:38 +0100

    changed card animation

--- src/components/templates/contentCard.js
+++ src/components/templates/contentCard.js
@@ -67,10 +67,15 @@ const cardContainerVariants = {
   visible: {
     opacity: 1,
     y: 0,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
   },
   hidden: {
     opacity: 0,
-    y: 50,
+    y: 0,
+    scale: 0,
   },
 };
 
