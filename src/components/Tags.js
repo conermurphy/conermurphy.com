1891af2e4cb18070881c5f39c38d040be809b0ef
Coner Murphy <coner@conermurphy.com>
2020-12-27 18:14:26 +0000

    refactored out tags component

---
+++ src/components/Tags.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import findTagInfo from '../utils/findTagInfo';
+
+const TagContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1.5rem;
+`;
+
+const TagStyle = styled.p`
+  margin: 0;
+  padding: 0.75rem 1rem;
+  font-size: 1.2rem;
+  background-color: var(--white);
+  border: 1px solid var(--green);
+`;
+
+export default function Tags({ frontmatter }) {
+  const { tags, id } = frontmatter;
+  return (
+    <TagContainer>
+      {tags.map((tag) => {
+        const { matchingTag, backgroundColor, color } = findTagInfo(tag);
+        return (
+          <TagStyle key={`PostTag-${id}-${matchingTag}`} backgroundColor={backgroundColor} color={color}>
+            {matchingTag}
+          </TagStyle>
+        );
+      })}
+    </TagContainer>
+  );
+}
