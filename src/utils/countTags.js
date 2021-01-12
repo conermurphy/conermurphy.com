d51ec178f7dd849bb83485c82bb721fb3ddd1974
Coner Murphy <coner@conermurphy.com>
2021-01-12 21:42:06 +0000

    updated packages

--- src/utils/countTags.js
+++ src/utils/countTags.js
@@ -8,8 +8,6 @@ export function arrayTotaler(type, arr) {
   } else if (type === 'blog') {
     totalTagArray = arr.blog.edges.map(({ node }) => node.frontmatter.tags).flat();
   } else if (type === 'portfolio') {
-    console.log(type);
-    console.log(arr);
     totalTagArray = arr.portfolio.edges.map(({ node }) => node.tags).flat();
   } else if (type === 'reads') {
     totalTagArray = arr.reads.edges.map(({ node }) => node.items[0].volumeInfo.categories).flat();
