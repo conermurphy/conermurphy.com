a14f2fc88523797135a8f7167fce988c0ff5b541
Coner Murphy <coner@conermurphy.com>
2021-01-12 22:58:03 +0000

    removed console logs

--- functions/twitter/twitter.js
+++ functions/twitter/twitter.js
@@ -25,8 +25,6 @@ async function getRequest() {
   // Setting query endpoint for API call.
   const queryEndPoint = `${endPoint}tweet.fields=referenced_tweets,public_metrics,entities&expansions=attachments.media_keys&media.fields=preview_image_url&max_results=${settings.count}`;
 
-  console.log(queryEndPoint);
-
   // Fetching the data from twitter and converting to JSON.
   const { data, includes } = await fetch(queryEndPoint, {
     method: 'GET',
@@ -60,7 +58,6 @@ exports.handler = async () => {
   try {
     // Get tweets and display output to page.
     const tweets = await getRequest();
-    console.log(tweets);
     return {
       statusCode: 200,
       headers: {
