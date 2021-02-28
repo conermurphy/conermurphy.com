0b830ab472e000b1c4e54bcf2562d9f0e7ba399b
Coner Murphy <coner@conermurphy.com>
2021-02-28 13:01:16 +0000

    added comments

--- src/utils/useEmail.js
+++ src/utils/useEmail.js
@@ -24,6 +24,7 @@ export default function useEmail({ values }) {
       chilliIsCool,
     };
 
+    // Checking there was an email entered.
     if (!email.length) {
       setLoading(false);
       setError(true);
