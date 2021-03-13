c0ce583f0a47987735050dc25852e1ddf8bc3763
Coner Murphy <coner@conermurphy.com>
2021-03-13 16:30:02 +0000

    removed all personal data being tracked by GA

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Layout from './src/components/Layout';
 import { NavThemeProvider } from './src/context/NavThemeContext';
-import checkForAnalyticsCookie from './src/utils/checkForAnalyticsCookie';
-import getFutureDate from './src/utils/getFutureDate';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
@@ -10,23 +8,4 @@ export function wrapPageElement({ element, props }) {
 
 export function wrapRootElement({ element }) {
   return <NavThemeProvider>{element}</NavThemeProvider>;
-}
-
-function gaOptout() {
-  const newExpiry = getFutureDate(new Date(), 30); // Cookie expres in 30 days.
-  const gaProperty = process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_ID;
-  const disableStr = `ga-disable-${gaProperty}`;
-  document.cookie.indexOf(`${disableStr}=true`) > -1 && (window[disableStr] = !0);
-  (document.cookie = `${disableStr}=true; expires=${newExpiry};path=/`), (window[disableStr] = !0);
-}
-
-export function onClientEntry() {
-  const analyticsAllowed = checkForAnalyticsCookie();
-
-  if (analyticsAllowed === 'false' || analyticsAllowed === 'promptUser') {
-    gaOptout();
-  }
-  if (analyticsAllowed === 'true') {
-    window.disableStr = '';
-  }
 }
--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { promptUserReconfirm } from '../utils/checkForAnalyticsCookie';
 import useNavTheme from '../utils/useNavTheme';
 
 const PrivacyPolicyContainer = styled.div`
@@ -33,11 +32,11 @@ export default function PrivacyPolicy() {
         <h1>Privacy Policy</h1>
       </div>
       <p>
-        <i>Last Updated: 04/01/2021</i>
+        <i>Last Updated: 13/03/2021</i>
       </p>
       <p>
-        If you wish to update your cookie consent choice,{' '}
-        <button onClick={() => promptUserReconfirm()} type="button">
+        If you wish to opt-out of Google Analytics,{' '}
+        <button href="window.gaOptout()" type="button">
           please click here.
         </button>{' '}
       </p>
@@ -65,9 +64,8 @@ export default function PrivacyPolicy() {
         address onto my email newsletter for future communication but this is completely optional.
       </p>
       <p>
-        Website analytics have also been included on this website in the form of Google Analytics but this is completely optional and as per
-        regulation is on an opt-in only basis. If you choose to ignore or decline the cookie popup shown on entry to the website then only
-        cookies stating the refusal are added. More information is given on this below.
+        Website analytics have also been included on this website in the form of Google Analytics. This has been configured with all
+        advertising, IP tracking, and demographics collection features turned off. No personal data is shared with Google Analytics.
       </p>
       <h2>Collection Method</h2>
       <p>There are 2 forms of data collection used on this website, these are detailed below:</p>
@@ -77,65 +75,32 @@ export default function PrivacyPolicy() {
         on this website their information ( email address ) would be shared and stored on ConvertKit's infrastructure. If you are interested
         in reading more about their privacy policy, <a href="https://convertkit.com/privacy">you can so here.</a>
       </p>
-      <p>
-        <strong>Automatic:</strong> Should a user accept the cookie policy displayed upon entry to this website then the relevant cookies
-        will be set on the user's browser to allow for communication with Google Analytics which is the analytics service being used on this
-        website. This can be reviewed at any time by{' '}
-        <button onClick={() => promptUserReconfirm()} type="button">
-          clicking here.
-        </button>
-      </p>
       <h2>Storage Information</h2>
       <p>
         This website does not use any internal databases for the storage of information collected on this website. But, data is stored by
         third parties with the user's consent. This is Google Analytics and ConvertKit, the relevant collected information is collected and
         stored on the correspondings companies infrastructure to allow for the carrying out of the desired service.
       </p>
-      <p>
-        Following this, information is stored into the user's browser via cookies. This is to record the user's choice in regards to the
-        collection of analytics on the website. By default, all analytics is disabled on the website until the user approves the collection
-        of the data. If the user ignores the cookie popup or declines the request then no analytics will be recorded.
-      </p>
       <p>Below is a breakdown of the cookies used on this website:</p>
       <ul>
         <li>
-          <strong>conermurphy.com_analytics=promptUser:</strong> This cookie is set by default when a new user visits the website and is
-          used to trigger the prompt for a reply to the cookie policy which then sets some of the below cookies depending on the decision.
-          If a user decides to change their response by using one of the buttons above this cookie will also be set to trigger the prompt.
-        </li>
-        <li>
-          <strong>conermurphy.com_analytics=false:</strong> If a user declines the use of analytics and cookies on this website then this
-          cookie is set so that on future visits the website will not prompt the user for confirmation and it will also opt the user
-          automatically out of Google Analytics on page load. This cookie lasts 30 days before expiring, if a user does not revisit within
-          30 days then they will be prompted for consent again on their next visit.
-        </li>
-        <li>
-          <strong>ga-disable-G-L047KBCSG4=true:</strong> If a user declines the use of analytics and cookies on this website then this
-          cookie is set so that on future visits the website will not prompt the user for confirmation and it will also opt the user
-          automatically out of Google Analytics on page load. This cookie is also set when the cookie "conermurphy.com_analytics=promptUser"
-          is being used to prevent the collection of analytics data.
-        </li>
-        <li>
-          <strong>conermurphy.com_analytics=true:</strong> If a user accepts the use of Google Analytics then this cookie is set to remember
-          this choice and allow for Google Analytics to collect data on this visit and subsequent ones. This cookie also has a 30 day
-          expiration time on it so if a user does not revisit within that time then they will be prompted for confirmation again on their
-          next visit.
-        </li>
-        <li>
-          <strong>conermurphy.com_ga=GA1.1.1065468216.1609791686:</strong> This cookie is automatically set by Google Analytics and is only
-          set once the user has accepted to the use of cookies on the website. If declined this cookie is never set on the page.
+          <strong>conermurphy.com_ga=GA1.1.1065468216.1609791686:</strong> This cookie is automatically set by Google Analytics.
         </li>
         <li>
           <strong>conermurphy.com_ga_L047KBCSG4=GS1.1.1609791685.1.1.1609791691.0:</strong> This cookie is automatically set by Google
-          Analytics and is only set once the user has accepted to the use of cookies on the website. If declined this cookie is never set on
-          the page.
+          Analytics.
         </li>
       </ul>
       <p>
-        Should you wish to change your choice in regards to cookies then you may do so by{' '}
-        <button onClick={() => promptUserReconfirm()} type="button">
-          clicking here.
+        If you wish to opt-opt of Google Analytics{' '}
+        <button href="window.gaOptout()" type="button">
+          please click here.
         </button>
+      </p>
+      <h2>Your Rights</h2>
+      <p>
+        You have the right to access, rectify, erase, restrict processing, object to processing, or export personal data you share with us.
+        If you would like to exercise these rights, please contact me.
       </p>
       <h2>Contact Details</h2>
       <p>Should you wish to change your preference for the use of cookies on this website, you may do it via one of the above links.</p>
