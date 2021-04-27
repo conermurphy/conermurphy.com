5366c226040690b5cc6793a3d889601b4c5af6b3
Coner Murphy <coner@conermurphy.com>
2021-04-27 06:11:39 +0100

    added services section and data

---
+++ src/components/Services.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FaLaptopCode } from 'react-icons/fa';
+import Services from '../data/services.json';
+
+const ServiceSectionContainer = styled.section`
+  background-color: var(--secondaryBg);
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  padding: 7rem;
+
+  & > div {
+    max-width: 1400px;
+    width: 100%;
+
+    & > h2,
+    h3 {
+      text-align: center;
+    }
+  }
+
+  .ServiceCardsContainer {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+    align-items: center;
+    gap: 4rem;
+    padding-top: 2rem;
+  }
+`;
+
+const ServiceCardContainer = styled.div`
+  background-color: var(--primaryBg);
+  filter: drop-shadow(var(--shadow));
+  border-radius: var(--borderRadius);
+  padding: 2.5rem;
+
+  .serviceCardTitle {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 1.5rem;
+
+    & > h4 {
+      margin: 0;
+      color: var(--primaryText);
+    }
+
+    & > svg {
+      background-color: var(--accent);
+      color: var(--primaryBg);
+      padding: 1rem;
+      border-radius: calc(var(--borderRadius) / 2);
+    }
+  }
+`;
+
+function ServiceCard({ service: { title, description, icon } }) {
+  let displayIcon;
+  switch (icon) {
+    case 'laptop':
+      displayIcon = <FaLaptopCode />;
+      break;
+    default:
+      displayIcon = '';
+  }
+
+  return (
+    <ServiceCardContainer>
+      <div className="serviceCardTitle">
+        {displayIcon}
+        <h4>{title}</h4>
+      </div>
+      <p>{description}</p>
+    </ServiceCardContainer>
+  );
+}
+
+export function ServiceSection() {
+  return (
+    <ServiceSectionContainer>
+      <div>
+        <h2 className="sectionTitle">Services</h2>
+        <h3 className="subtitle">It's more than just a website...</h3>
+        <div className="ServiceCardsContainer">
+          {Services.map((service) => (
+            <ServiceCard service={service} key={service.title} />
+          ))}
+        </div>
+      </div>
+    </ServiceSectionContainer>
+  );
+}
