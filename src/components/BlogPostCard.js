d5a01c1009789e9245fc47b9047c993a987d5a3f
Coner Murphy <coner@conermurphy.com>
2021-05-05 17:30:18 +0100

    updated blog post card

--- src/components/BlogPostCard.js
+++ src/components/BlogPostCard.js
@@ -1,118 +1,38 @@
-import { Link } from 'gatsby';
+import { graphql, useStaticQuery, Link } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
 import React from 'react';
 import styled from 'styled-components';
-import { GatsbyImage } from 'gatsby-plugin-image';
-import PropTypes from 'prop-types';
 
-const PostContainerBody = styled.div`
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-  box-shadow: var(--shadow);
+const LatestPostContainer = styled.div`
+  max-width: 400px;
 
   .gatsby-image-wrapper {
-    width: 100%;
-    overflow: visible;
+    max-width: 400px;
+    border-radius: var(--borderRadius);
   }
 
-  .contentContainer {
-    display: grid;
-    grid-template-rows: auto 1fr;
-    flex: 1;
-    padding: 2rem;
-    padding-bottom: 1rem;
-    align-items: center;
-    justify-items: center;
-    text-align: center;
-
-    .languageIconContainer {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
-      margin-top: -5rem;
-      z-index: 5;
-
-      svg {
-        padding: 1rem;
-        border-radius: var(--borderRadius);
-        background-color: var(--white);
-        -moz-box-shadow: inset 0px 0px 4px rgba(0, 0, 0, 0.12);
-        -webkit-box-shadow: inset 0px 0px 4px rgba(0, 0, 0, 0.12);
-        box-shadow: inset 0px 0px 4px rgba(0, 0, 0, 0.12);
-      }
-    }
-
-    & > .content {
-      margin: 1.5rem 0;
-
-      h2 {
-        font-size: 1.75rem;
-        padding: 1rem 0;
-      }
-
-      p {
-        font-size: 1.5rem;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        margin-top: 0;
-      }
-    }
-  }
-
-  .postIdDate {
-    display: grid;
-    grid-template-columns: repeat(2, 50%);
-    align-items: center;
-    justify-items: center;
-    border-top: 0.25rem solid var(--grey);
-
-    p {
-      display: flex;
-      align-items: center;
-      font-size: 1.5rem;
-      height: 100%;
-      font-weight: bold;
-    }
+  .published {
+    font-size: 1.25rem;
+    margin: 0.5rem 0;
+    font-weight: 300;
   }
 `;
 
-export default function BlogPostCard({ post }) {
-  const { frontmatter, fields } = post.node;
-  const { date, id, image, tags, title } = frontmatter;
+export function BlogPostCard({ post }) {
+  // 1: Destructure out data
+  const { excerpt, frontmatter, fields } = post;
+  const { date, description, title, image } = frontmatter;
   const { slug } = fields;
 
+  // 2: Return the card
   return (
-    <Link to={slug}>
-      <PostContainerBody>
-        <GatsbyImage image={image.childImageSharp.gatsbyImageData} />
-        <div className="contentContainer">
-          <div className="content">
-            <h2>{title}</h2>
-          </div>
-        </div>
-        <div className="postIdDate">
-          <p className="id">Post: #{id}</p>
-          <p>{date}</p>
-        </div>
-      </PostContainerBody>
-    </Link>
+    <LatestPostContainer>
+      <Link to={slug} className="postLinks">
+        <GatsbyImage image={image.childImageSharp.gatsbyImageData} alt={title} />
+        <p className="published">Published on {date}</p>
+        <h3 className="title">{title}</h3>
+        {description !== '' ? <p>{description}</p> : <p>{excerpt}</p>}
+      </Link>
+    </LatestPostContainer>
   );
 }
-
-BlogPostCard.propTypes = {
-  post: PropTypes.shape({
-    node: PropTypes.shape({
-      frontmatter: PropTypes.shape({
-        date: PropTypes.string,
-        id: PropTypes.number,
-        image: PropTypes.object,
-        tags: PropTypes.array,
-        title: PropTypes.string,
-      }),
-      fields: PropTypes.shape({
-        slug: PropTypes.string.isRequired,
-      }),
-    }),
-  }),
-};
