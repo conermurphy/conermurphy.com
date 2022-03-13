d75a46898c380a7ab5af802e025ebed51cacb1c9
Coner Murphy <coner@conermurphy.com>
2022-03-13 12:30:28 +0000

    Feature/19 add custom fonts styles (#20)
    * feat(#19): add custom fonts and styles - added custom fonts to app and add custom global styles as base
    
    * chore(#19): add custom fonts styles - updated tailwindcss config for custom fonts and colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        offWhite: '#FAFAFC',
+        primary: '#111827',
+        accent: '#F97316',
+      },
+    },
+    fontFamily: {
+      display: ['Poppins'],
+      body: ['Poppins'],
+    },
   },
   plugins: [],
-}
+};
