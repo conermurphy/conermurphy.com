e847afd05e541f27e2925cd6a3b61c6a4c9b046d
Coner Murphy <coner@conermurphy.com>
2022-03-17 06:25:08 +0000

    Feature/24 create services component (#47)
    * feat(#24): create services component - added services component
    
    * chore(#24): create services component - updated HTML tags and tests

---
+++ components/Services/test.spec.tsx
@@ -0,0 +1,36 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import Services from './Services';
+
+const services = [
+  {
+    title: 'Service Title 1',
+    copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla arcu at facilisis dignissim arcu pretium tempus. Cras scelerisque urna amet sagittis donec arcu ipsum.',
+    icon: 'keyboard',
+  },
+  {
+    title: 'Service Title 2',
+    copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla arcu at facilisis dignissim arcu pretium tempus. Cras scelerisque urna amet sagittis donec arcu ipsum.',
+    icon: 'code',
+  },
+  {
+    title: 'Service Title 3',
+    copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla arcu at facilisis dignissim arcu pretium tempus. Cras scelerisque urna amet sagittis donec arcu ipsum.',
+    icon: 'socials',
+  },
+];
+
+describe('Services', () => {
+  it('Matches the snapshot', () => {
+    const { container } = render(<Services services={services} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Should have 3 child elements', () => {
+    const { container } = render(<Services services={services} />);
+    const ul = container.querySelector('ul');
+    const items = ul?.querySelectorAll('article');
+
+    expect(items).toHaveLength(3);
+  });
+});
