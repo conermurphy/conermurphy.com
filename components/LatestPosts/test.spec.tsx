147d43edd8558c33835805b1317ad5dd5b7f795e
Coner Murphy <coner@conermurphy.com>
2022-03-26 14:12:23 +0000

    feat(#26): latest posts component - added latest posts and updated types

---
+++ components/LatestPosts/test.spec.tsx
@@ -0,0 +1,70 @@
+import { render, screen } from '@testing-library/react';
+import LatestPosts from './LatestPosts';
+
+const mockData = [
+  {
+    data: {
+      id: 1,
+      title: 'Example Post 1',
+      date: '2022-03-26',
+      tags: ['JAVASCRIPT'],
+      slug: '/example-post-1',
+      image:
+        '/images/blog/complete-2021-guide-to-http-status-codes-their-seo-influence.png',
+      description: 'This is a description of a post',
+      categories: [''],
+      published: true,
+      canonical_url: '',
+    },
+  },
+  {
+    data: {
+      id: 2,
+      title: 'Example Post 1',
+      date: '2022-03-26',
+      tags: ['JAVASCRIPT'],
+      slug: '/example-post-1',
+      image:
+        '/images/blog/complete-2021-guide-to-http-status-codes-their-seo-influence.png',
+      description: 'This is a description of a post',
+      categories: [''],
+      published: true,
+      canonical_url: '',
+    },
+  },
+  {
+    data: {
+      id: 3,
+      title: 'Example Post 1',
+      date: '2022-03-26',
+      tags: ['JAVASCRIPT'],
+      slug: '/example-post-1',
+      image:
+        '/images/blog/complete-2021-guide-to-http-status-codes-their-seo-influence.png',
+      description: 'This is a description of a post',
+      categories: [''],
+      published: true,
+      canonical_url: '',
+    },
+  },
+];
+
+describe('LatestPosts', () => {
+  it('should have correct title and subtitle', () => {
+    render(<LatestPosts posts={mockData} />);
+
+    const title = screen.queryByText(/Latest Content.../i);
+    const subtitle = screen.queryByText(/What I’m up too and more./i);
+
+    expect(title).toBeVisible();
+    expect(subtitle).toBeVisible();
+  });
+
+  it('should match the length provided', () => {
+    render(<LatestPosts posts={mockData} />);
+
+    const posts = screen.queryAllByText('Example Post 1');
+
+    expect(posts).toHaveLength(3);
+  });
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,35 @@
 import type { GetStaticProps, NextPage } from 'next';
-import { Services, Spotlight, Testimonials } from '../components';
-import { Service, SPOTLIGHT, Testimonial } from '../types';
+import { LatestPosts, Services, Spotlight, Testimonials } from '../components';
+import { Posts, Service, SPOTLIGHT, Testimonial } from '../types';
 import { pageDataSource } from '../utils';
 
 interface IProps {
   services: Service[];
   testimonials: Testimonial[];
+  posts: Posts;
 }
 
-const Home: NextPage<IProps> = ({ services, testimonials }) => {
+const Home: NextPage<IProps> = ({ services, testimonials, posts }) => {
   return (
     <>
       <Spotlight type={SPOTLIGHT.COMPANIES} />
       <Services services={services} />
       <Spotlight type={SPOTLIGHT.TECH} />
+      <LatestPosts posts={posts} />
       <Testimonials testimonials={testimonials} />
     </>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { services, testimonials } = await pageDataSource({
+  const { services, testimonials, posts } = await pageDataSource({
     services: true,
     testimonials: true,
+    latestPosts: true,
   });
 
   return {
-    props: { services, testimonials },
+    props: { services, testimonials, posts },
   };
 };
 
