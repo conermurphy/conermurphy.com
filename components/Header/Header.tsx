3f6a943025784f4874bfc1e96fbc2fcf8478052d
Coner Murphy <coner@conermurphy.com>
2022-04-02 12:21:36 +0100

    fix(#65): header title link home - made header title into link to home and updated tests

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
 
@@ -6,7 +7,9 @@ export default function Header(): JSX.Element {
     <div className="sticky top-0 z-20">
       <header className="flex flex-row items-center justify-between h-16 px-106 bg-transparent">
         <div className="flex flex-row items-center gap-12">
-          <p className="text-2xl font-bold opacity-100">Coner Murphy</p>
+          <Link href="/" passHref>
+            <a className="text-2xl font-bold opacity-100">Coner Murphy</a>
+          </Link>
           <NavBar />
         </div>
       </header>
