f60eee38e8178a9f678ad021b6c4ad1941cfae25
Coner Murphy <coner@conermurphy.com>
2022-04-02 12:08:25 +0100

    fix(#55): footer breakpoint issues - updated ternaries in Footer component to match tailwind requirements (#69)

--- components/Newsletter/Newsleter.tsx
+++ components/Newsletter/Newsleter.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useEmail, useForm } from '../../utils';
 
 interface IProps {
-  breakpoint?: string;
+  breakpoint?: 'md' | 'lg';
 }
 
 export default function Newsletter({ breakpoint = 'md' }: IProps): JSX.Element {
@@ -15,7 +15,11 @@ export default function Newsletter({ breakpoint = 'md' }: IProps): JSX.Element {
 
   return (
     <div
-      className={`flex flex-col gap-3 max-w-[272px] ${breakpoint}:max-w-[372px] ${breakpoint}:gap-4`}
+      className={`flex flex-col gap-3 max-w-[272px] ${
+        breakpoint === 'md'
+          ? 'md:max-w-[372px] md:gap-4'
+          : 'lg:max-w-[372px] lg:gap-4'
+      }`}
     >
       <div>
         <p className="font-bold opacity-100">Stay up to date</p>
@@ -26,7 +30,9 @@ export default function Newsletter({ breakpoint = 'md' }: IProps): JSX.Element {
       </div>
       <form
         onSubmit={submitEmail}
-        className={`flex flex-col gap-y-2 gap-x-4 ${breakpoint}:flex-row`}
+        className={`flex flex-col gap-y-2 gap-x-4 ${
+          breakpoint === 'md' ? 'md:flex-row' : 'lg:flex-row'
+        }`}
         data-testid="newsletter-form"
       >
         <input
@@ -37,7 +43,9 @@ export default function Newsletter({ breakpoint = 'md' }: IProps): JSX.Element {
           placeholder="Enter your email"
           onChange={updateValue}
           value={values.email}
-          className={`rounded-lg border-primaryBorder max-w-[272px] text-xs bg-primaryBg ${breakpoint}:text-md`}
+          className={`rounded-lg border-primaryBorder max-w-[272px] text-xs bg-secondaryBg ${
+            breakpoint === 'md' ? 'md:text-md' : 'lg:text-md'
+          }`}
         />
         <input
           type="text"
@@ -48,12 +56,20 @@ export default function Newsletter({ breakpoint = 'md' }: IProps): JSX.Element {
         />
         <button
           type="submit"
-          className={`bg-primaryText text-primaryBg text-xs rounded-lg py-3 px-5 ${breakpoint}:text-md`}
+          className={`bg-primaryText text-primaryBg text-xs rounded-lg py-3 px-5 ${
+            breakpoint === 'md' ? 'md:text-md' : 'lg:text-md'
+          }`}
         >
           {loading ? 'Subscribing..' : 'Subscribe'}
         </button>
       </form>
-      <p className={`text-xs ${breakpoint}:text-sm`}>{message}</p>
+      <p
+        className={`text-xs ${
+          breakpoint === 'md' ? 'md:text-sm' : 'lg:text-sm'
+        }`}
+      >
+        {message}
+      </p>
     </div>
   );
 }
