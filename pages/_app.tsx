cd0cbae8a9fcf3e2627d584b56e500b82a773f81
Coner Murphy <coner@conermurphy.com>
2022-04-06 18:14:44 +0100

    feat(#74): setup fathom analytics - installed and configured fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,36 @@
 import '../styles/globals.css';
 import '../styles/cobalt2.prism.css';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load('FBDCDFEY', {
+      includedDomains: ['conermurphy.com, www.conermurphy.com'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
