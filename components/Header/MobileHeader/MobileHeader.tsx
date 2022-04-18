1df46e7a2a24b8bb16fda72a446398c89a9a9fb3
Coner Murphy <coner@conermurphy.com>
2022-04-18 08:12:53 +0100

    fix(#103): mobile nav close outside click - added custom hook

--- components/Header/MobileHeader/MobileHeader.tsx
+++ components/Header/MobileHeader/MobileHeader.tsx
@@ -1,26 +1,33 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { ICONS } from '../../../constants';
-import { getIcon } from '../../../utils';
+import { getIcon, useOutsideClick } from '../../../utils';
 import { MobileNavMenu } from './components';
 
 export default function MobileHeader(): JSX.Element {
   const { asPath } = useRouter();
+  const headerRef = useRef(null);
   const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
+  const handleIsOpen = useCallback(() => {
     setIsOpen((i) => {
       if (i) {
         return !i;
       }
       return i;
     });
-  }, [asPath]);
+  }, []);
+
+  useEffect(() => {
+    handleIsOpen();
+  }, [asPath, handleIsOpen]);
+
+  useOutsideClick({ ref: headerRef, callback: handleIsOpen });
 
   return (
     <div className="sticky top-0 z-20 block lg:hidden">
-      <header className="m-auto">
+      <header className="m-auto" ref={headerRef}>
         <div className="flex flex-row items-center justify-between h-16 bg-transparent px-6 sm:px-20">
           <Link href="/" passHref>
             <a className="font-bold opacity-100">Coner Murphy</a>
