e026906f7bcc39402a77605d43f1e7ad29a588ff
Coner Murphy <coner@conermurphy.com>
2022-04-21 10:03:39 +0100

    feat(#81): sitemap - added sitemap generation (#109)
    * feat(#81): sitemap - added sitemap generation
    
    * chore(#81): sitemap - removed local generated files

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
+const dev = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  siteUrl: dev ? 'http://localhost:3000' : 'https://conermurphy.com',
+  generateRobotsTxt: true,
+};
