3ba23157de7338b05b29b29448ae0ea60a4f0464
Coner Murphy <coner@conermurphy.com>
2022-04-23 21:07:20 +0100

    fix(#129): services icon width - updated text size to accomodate icon (#144)

--- components/Services/Services.tsx
+++ components/Services/Services.tsx
@@ -24,12 +24,12 @@ export default function Services({ services }: IProps): JSX.Element {
           });
           return (
             <li key={title}>
-              <article className="flex flex-col gap-6 h-full justify-between md:max-w-[360px] bg-primaryBg rounded-lg px-8 py-6">
+              <article className="flex flex-col gap-6 h-full justify-between md:max-w-[360px] bg-primaryBg rounded-lg px-6 sm:px-8 py-6">
                 <div className="flex flex-row items-center gap-3">
                   <div className="w-10 h-10 flex items-center justify-center bg-primaryText text-primaryBg rounded-lg">
                     {iconSVG}
                   </div>
-                  <h3 className="text-lg font-semibold">{title}</h3>
+                  <h3 className="text-sm sm:text-lg font-semibold">{title}</h3>
                 </div>
                 <p className="text-sm md:text-base">{copy}</p>
               </article>
