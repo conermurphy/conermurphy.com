edf39f65a0256cf4799f49efa9242daabd645b94
Coner Murphy <coner@conermurphy.com>
2022-09-01 18:51:24 +0100

    chore(#230): finished adding missing newsletters

--- components/Post/PostCard/PostCard.tsx
+++ components/Post/PostCard/PostCard.tsx
@@ -22,7 +22,7 @@ export default function PostCard({ post, postType }: IProps): JSX.Element {
       className="contents"
     >
       <NoScrollLink href={`/${postType}/${slug}`} passHref>
-        <a className="group relative flex flex-col max-w-xs lg:max-w-sm cursor-pointer bg-secondaryBg dark:bg-secondaryBgDark rounded-md overflow-hidden h-full min-w-full sm:min-w-0">
+        <a className="group relative flex flex-col max-w-xs lg:max-w-sm cursor-pointer bg-secondaryBg dark:bg-secondaryBgDark rounded-md overflow-hidden h-full w-full min-w-full sm:min-w-0">
           <article>
             <div className="absolute h-full w-full items-center justify-center flex group-hover:border-2 border-accent transition-all duration-300 ease-in-out z-10 bg-primaryBgDark/50 group-hover:opacity-100 opacity-0 rounded-md">
               <p className="flex flex-row gap-2 items-center bg-accent px-4 py-2 font-bold rounded-sm">
