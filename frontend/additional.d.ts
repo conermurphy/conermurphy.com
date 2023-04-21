32545dacd582a70d8be2bb84f006a1cd17573a11
Coner Murphy <coner@conermurphy.com>
2023-04-21 20:33:41 +0100

    fix: resolved issues with ignoring files

---
+++ frontend/additional.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="next-images" />
+
+declare global {
+    namespace NodeJS {
+      interface ProcessEnv {
+        CONVERTKIT_ENDPOINT: string;
+        CONVERTKIT_PUBLIC_KEY: string;
+        CONVERTKIT_SIGNUP_FORM: string;
+        NEXT_PUBLIC_WPM: string;
+        POSTS_PER_PAGE: string;
+        CONTACT_FORM_ENDPOINT: string;
+        CONTACT_FORM_API_KEY: string;
+        APPSYNC_API_URL: string;
+        APPSYNC_API_KEY: string;
+        AWS_API_REGION: string;
+        APPSYNC_API_AUTH_TYPE: 'API_KEY';
+      }
+    }
+  }
+  
+  export {};
