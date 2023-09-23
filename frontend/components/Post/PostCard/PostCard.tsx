eb28a7e64994e90e249fc170da4ce54dc668977a
Coner Murphy <coner@conermurphy.com>
2023-09-23 10:53:09 +0100

    feat: added time element to post page

--- frontend/components/Post/PostCard/PostCard.tsx
+++ frontend/components/Post/PostCard/PostCard.tsx
@@ -19,6 +19,12 @@ export default function PostCard({ post, postType }: IProps): JSX.Element {
 
   const isTechnicalWriting = postType === POSTTYPES.TECHNICAL_WRITING;
 
+  const postsDateUI = new Date(date).toLocaleDateString('en-GB', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+
   return (
     <motion.article
       initial="offscreen"
@@ -31,13 +37,13 @@ export default function PostCard({ post, postType }: IProps): JSX.Element {
         href={!isTechnicalWriting ? `/${postType}/${slug}` : post.canonical_url}
       >
         <div className="flex flex-col gap-y-4 p-8 break-words border-text/10 hover:border-brand border-l-8 max-w-lg h-full justify-center duration-300 ease-in-out transition-all">
-          <p className="text-brand font-heading font-extrabold text-lg">
-            {new Date(date).toLocaleDateString('en-GB', {
-              year: 'numeric',
-              month: 'short',
-              day: 'numeric',
-            })}
-          </p>
+          <time
+            className="text-brand font-heading font-extrabold text-lg"
+            dateTime={date}
+          >
+            {postsDateUI}
+          </time>
+
           <div className="flex flex-col gap-2">
             <h3
               className="text-lg md:text-xl lg:text-2xl font-heading text-text/90"
