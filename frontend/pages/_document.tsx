9d0633cba23a493a83bf860597b242c433d36a09
Coner Murphy <coner@conermurphy.com>
2024-03-31 16:02:20 +0100

    chore: update linting

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,5 +1,5 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
-import React from "react";
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import React from 'react';
 
 export default class MyDocument extends Document {
   render() {
